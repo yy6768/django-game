87b190c4208dc77f305f1b61f2b9661e7adf59ec
yy6768 <1258720926@qq.com>
2022-11-18 15:54:09 +0800

    js canvaso¼

---
+++ game/static/js/src/playground/player/zbase.js
@@ -0,0 +1,165 @@
+class Player extends PyGameObject {
+    constructor(playground, x, y, radius,color, speed, is_me){
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx; 
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.vx = 0;
+        this.vy = 0;
+        this.damage_x = 0;
+        this.damage_y = 0;
+        this.damage_speed = 0;
+        this.move_length = 0;
+        this.eps = 0.1;
+
+        //冷静期
+        this.spent_time = 0;
+
+        //摩擦力
+        this.friction = 0.9;
+
+        this.cur_skill = null;
+    }
+
+    start(){
+        if(this.is_me){
+            this.add_listening_events();
+        } else {
+            let tx = Math.random() * this.playground.width;
+            let ty = Math.random() * this.playground.height;
+            this.move_to(tx,ty);
+        }
+    }
+
+    add_listening_events(){
+        this.playground.game_map.$canvas.on("contextmenu", function(){
+            return false;
+        });
+        let that = this;
+        this.playground.game_map.$canvas.mousedown(function(e){
+            if(e.which ===3){
+                that.move_to(e.clientX,e.clientY);
+            } else if(e.which === 1){
+                if(that.cur_skill === "fireball"){
+                    that.shoot_fireball(e.clientX, e.clientY);
+                }
+
+                that.cur_skill = null;
+            }
+        });
+
+        $(window).keydown(function(e){ 
+            if(e.which === 81) {
+                that.cur_skill = "fireball";
+                return false;
+            }
+        });
+    }
+
+    shoot_fireball(tx ,ty){
+        let x = this.x, y = this.y;
+        let radius = this.playground.height * 0.01;
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        let vx = Math.cos(angle), vy = Math.sin(angle);
+        let color = "orange";
+        let speed = this.playground.height * 0.5;
+        let move_length = this.playground.height * 1;
+        new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length, this.playground.height * 0.01);
+    }
+
+    get_dist(x1,x2,y1,y2){
+        let dx = x1 - x2;
+        let dy = y1 - y2;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    move_to(tx , ty){
+        this.move_length = this.get_dist(this.x, tx, this.y, ty);
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        this.vx = Math.cos(angle);
+        this.vy = Math.sin(angle);
+    }
+
+    is_attacked(angle,damage){
+        for(let i = 0;  i < 20 + Math.random() * 10; i++){
+            let x = this.x;
+            let y = this.y;
+            let radius = this.radius * Math.random() * 0.1;
+            let angle = Math.random() *  Math.PI *2;
+            let vx = Math.cos(angle), vy = Math.sin(angle);
+            let color = this.color;
+            let speed = this.speed * 10;
+            let move_length = this.radius * Math.random() * 5;
+            new Partical(this.playground, x, y, radius, vx, vy, color, speed, move_length);
+        }
+        this.radius -= damage;
+        if(this.radius < 10){
+            this.destroy();
+            return false;
+        }
+        this.damage_x = Math.cos(angle);
+        this.damage_y = Math.sin(angle);
+        this.damage_speed = damage * 100;
+        this.speed *= 0.8;
+
+
+    }
+
+    update(){
+        this.spent_time += this.timedelta / 1000;
+
+        if(!this.is_me && this.spent_time > 4 && Math.random() < 1 / 300.0){
+            let player = this.playground.players[Math.floor(Math.random() * this.playground.players.length)];
+            let tx = player.x + player.vx * player.speed * this.timedelta / 1000 * 0.3;
+            let ty = player.y + player.vy * player.speed * this.timedelta / 1000 * 0.3;
+            this.shoot_fireball(tx, ty);
+        }
+
+        if(this.damage_speed > 10){
+            this.vx = this.vy = 0;
+            this.move_length = 0;
+            this.x += this.damage_x * this.damage_speed * this.timedelta / 1000;
+            this.y += this.damage_y * this.damage_speed * this.timedelta / 1000;
+            this.damage_speed *= this.friction;
+        }else{
+            if(this.move_length < this.eps){
+                this.move_length = 0;
+                this.vx = 0;
+                this.vy = 0;
+                if(!this.is_me){
+                    let tx = Math.random() * this.playground.width;
+                    let ty = Math.random() * this.playground.height;
+                    this.move_to(tx,ty);
+                }
+            } else {
+                let moved = Math.min(this.speed * this.timedelta / 1000, this.move_length);
+                this.x += this.vx * moved;
+                this.y += this.vy * moved;
+                this.move_length -= moved;
+            }
+        }
+        this.render();
+
+    }
+
+    render(){
+
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+
+    on_destroy(){
+        for(let i = 0; i < this.playground.players.length; i++){
+            if(this.playground.players[i] === this){
+                this.playground.players.splice(i, 1);
+            }
+        }
+    }
+}
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,14 +1,28 @@
 class PyGamePlayGround{
     constructor(root){
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
-        this.hide();
+        this.$playground = $(`<div class="py-game-playground"></div>`);
+        // this.hide();
         this.root.$py_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width/2, this.height/2, this.height * 0.05, "white", this.height*0.15, true));
+        for(let i = 0; i < 5; i++){
+            this.players.push(new Player(this, this.width/2, this.height/2, this.height * 0.05, this.get_random_color(), this.height*0.15, false));
+        }
         this.start();
     }
 
     start(){
         
+    }
+
+    get_random_color(){
+        let colors = ["blue", "red", "pink", "grey", "green"];
+        return colors[Math.floor(Math.random() * 5)];
     }
     //打开playground界面
     show(){
